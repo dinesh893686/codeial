ddc6df532109db761d811242b2631975363ae5b4
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-05 00:16:11 +0530

    created view for users profile and users posts

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,9 +1,17 @@
 module.exports.profile=function(req,res)
 {
-    res.end("<h1>Profile Created</h1>")
+    // res.end("<h1>Profile Created</h1>")
+    return res.render('./user/user_profile',{
+        title:"user profile",
+        name:"Dinesh"
+    })
 }
 module.exports.posts=function(req,res)
 {
-    res.end("<h1>User Posts Created<h1>")
+    // res.end("<h1>User Posts Created<h1>")
+    return res.render('./user/user_posts',{
+        title:"user posts",
+        posts:"story"
+    })
 
 }
