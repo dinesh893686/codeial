41e515e8a5780be9e91d5311fdb74bc2765655f2
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-04 23:37:40 +0530

    created view for home using ejs

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,8 @@
 module.exports.home=function(req,res)
 {
-    res.end('<h1>Codeial is up and running</h1>')
+   // res.end('<h1>Codeial is up and running</h1>')
+   return res.render('home',
+   {
+       title:"Home"
+   })
 }
