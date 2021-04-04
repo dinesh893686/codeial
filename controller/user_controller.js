a069a9479df5a98a192b8d717eead01c2c4e644a
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-04 21:15:02 +0530

    created routes and controller for user posts

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -2,3 +2,8 @@ module.exports.profile=function(req,res)
 {
     res.end("<h1>Profile Created</h1>")
 }
+module.exports.posts=function(req,res)
+{
+    res.end("<h1>User Posts Created<h1>")
+
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const user_controller=require('../controller/user_controller');
 
 router.get('/profile',user_controller.profile);
+ router.get('/posts',user_controller.posts);
 
 
 module.exports=router;
