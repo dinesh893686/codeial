ffd7843d6c24b137431a7a849dfbda215a9d5cf9
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-07 09:35:10 +0530

    created partials for home user_ profile user_posts

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const user_controller=require('../controller/user_controller');
 
 router.get('/profile',user_controller.profile);
- router.get('/posts',user_controller.posts);
+router.get('/posts',user_controller.posts);
 
 
 module.exports=router;
